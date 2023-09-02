5569d4aaf04a3e7023187ac360aeba1feae3078f
Ruslan-Developer <ruslan-developer@developerlab.pp.ru>
2023-09-02 13:34:57 +0300

    Made an implementation of the cache based on the list

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,17 +9,66 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
+type Node struct {
+	Key   Key
+	Value interface{}
+}
+
+// NewCache Создать новый кэш.
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+// Set Добавить элемент в кэш по указанному ключу.
+func (elem *lruCache) Set(key Key, value interface{}) bool {
+	if item, ok := elem.items[key]; !ok {
+		elem.items[key] = elem.queue.PushFront(&Node{
+			Key:   key,
+			Value: value,
+		})
+
+		if elem.queue.Len() > elem.capacity {
+			lastItem := elem.queue.Back()
+
+			if nodeItem, nodeOk := lastItem.Value.(*Node); nodeOk {
+				delete(elem.items, nodeItem.Key)
+				elem.queue.Remove(lastItem)
+			}
+		}
+	} else if nodeItem, nodeOk := item.Value.(*Node); nodeOk {
+		nodeItem.Value = value
+		elem.queue.MoveToFront(item)
+
+		return true
+	}
+
+	return false
+}
+
+// Get Получить элемент из кэша по указанному ключу.
+func (elem *lruCache) Get(key Key) (interface{}, bool) {
+	if item, ok := elem.items[key]; ok {
+		elem.queue.MoveToFront(item)
+
+		if nodeItem, nodeOk := item.Value.(*Node); nodeOk {
+			return nodeItem.Value, ok
+		}
+	}
+
+	return nil, false
+}
+
+// Clear Очистить кэш.
+func (elem *lruCache) Clear() {
+	elem.queue = NewList()
+	elem.items = make(map[Key]*ListItem, elem.capacity)
 }
