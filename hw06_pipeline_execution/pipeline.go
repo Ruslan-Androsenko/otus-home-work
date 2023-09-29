740a7a0395f20bb24e461f5ba8ead8cc10b992f8
Ruslan-Androsenko <ruslan-developer@developerlab.pp.ru>
2023-09-29 09:25:49 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,34 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	var (
+		out         = in
+		emptyOut    = make(Bi)
+		countStages = len(stages)
+	)
+	close(emptyOut)
+
+	// Если стейджи отсутствуют, сразу выходим из функции
+	if countStages == 0 {
+		return emptyOut
+	}
+
+	for _, stage := range stages {
+		// Если done-канал не был передан, то не прослушиваем его
+		if done != nil {
+			for {
+				select {
+				case <-done:
+					return emptyOut
+				default:
+					out = stage(out)
+					break
+				}
+			}
+		} else {
+			out = stage(out)
+		}
+	}
+
+	return out
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -90,4 +90,63 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+
+	t.Run("empty stages", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		var emptyStages []Stage
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, nil, emptyStages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Len(t, result, 0)
+		require.Less(t, int64(elapsed), int64(fault))
+	})
+
+	t.Run("additional stages", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		countStages := len(stages)
+		additionalStages := stages[:countStages-1]
+		additionalStages = append(additionalStages,
+			g("Multiplier (* 4)", func(v interface{}) interface{} { return v.(int) * 4 }),
+			g("Adder (+ 512)", func(v interface{}) interface{} { return v.(int) + 512 }),
+			g("Divider (/ 8)", func(v interface{}) interface{} { return v.(int) / 8 }),
+			g("Subtraction (- 42)", func(v interface{}) interface{} { return v.(int) - 42 }),
+
+			stages[countStages-1],
+		)
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, nil, additionalStages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Equal(t, []string{"73", "74", "75", "76", "77", "78", "79", "80", "81", "82"}, result)
+		require.Less(t,
+			int64(elapsed),
+			// ~1.7s for processing 10 values in 8 stages (100ms every) concurrently
+			int64(sleepPerStage)*int64(len(additionalStages)+len(data)-1)+int64(fault))
+	})
 }
