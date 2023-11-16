bfeef77698d5ffe88ce4cc239d223ec5b98ca7e6
Ruslan-Androsenko <ruslan-developer@developerlab.pp.ru>
2023-11-16 23:53:47 +0300

    HW11 Implemented functionality for the telnet client

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,99 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var timeout time.Duration
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "timeout of closing")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	if len(os.Args) < 3 {
+		log.Fatalln("Required arguments has missing (host or port)")
+	}
+
+	ctxNotify, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// Получаем из аргументов адрес хоста и порт.
+	countArgs := len(os.Args)
+	address := net.JoinHostPort(os.Args[countArgs-2], os.Args[countArgs-1])
+	telnet := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
+	defer func() {
+		err := telnet.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	err := telnet.Connect()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Запускаем горутину для отправки данных в сокет.
+	go func() {
+		for {
+			select {
+			case <-ctxNotify.Done():
+				return
+			default:
+				err = telnet.Send()
+				if hasBeenError(err) {
+					log.Printf("Failed send data to socket, %v", err)
+				}
+
+				stopGoroutinesIfEndOfFile(err, stop)
+			}
+		}
+	}()
+
+	// Запускаем горутину для получения данных из сокета.
+	go func() {
+		for {
+			select {
+			case <-ctxNotify.Done():
+				return
+			default:
+				err = telnet.Receive()
+				if hasBeenError(err) {
+					log.Printf("Failed receive data from socket, %v", err)
+				}
+
+				stopGoroutinesIfEndOfFile(err, stop)
+			}
+		}
+	}()
+
+	<-ctxNotify.Done()
+	errNotify := ctxNotify.Err()
+	if hasBeenError(errNotify) {
+		log.Println(errNotify)
+	}
+}
+
+// Проверяем что это пришла ошибка а не конец файла.
+func hasBeenError(err error) bool {
+	return err != nil && !errors.Is(err, io.EOF)
+}
+
+// Остановить горутины если ошибка является концом файла.
+func stopGoroutinesIfEndOfFile(err error, stop context.CancelFunc) {
+	if errors.Is(err, io.EOF) {
+		stop()
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
+	"net"
 	"time"
+)
+
+const (
+	messageByClient = "Bye, client!\n"
+	messageByServer = "Bye-bye\n"
 )
 
 type TelnetClient interface {
@@ -13,9 +21,92 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &Telnet{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type Telnet struct {
+	conn    net.Conn
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+}
+
+func (t *Telnet) Connect() error {
+	conn, err := net.DialTimeout("tcp", t.address, t.timeout)
+	if err != nil {
+		return fmt.Errorf("cannot connect: %w", err)
+	}
+
+	t.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *Telnet) Close() error {
+	err := t.in.Close()
+	if err != nil {
+		return fmt.Errorf("cannot closing input: %w", err)
+	}
+
+	err = t.conn.Close()
+	if err != nil {
+		return fmt.Errorf("cannot closing connect: %w", err)
+	}
+
+	return nil
+}
+
+func (t *Telnet) Send() error {
+	errRead := readAndWrite(t.in, t.conn)
+	if errors.Is(errRead, io.EOF) {
+		errWrite := sendExitMessage(t.conn, messageByServer)
+		if errWrite != nil {
+			return errWrite
+		}
+	}
+
+	return errRead
+}
+
+func (t *Telnet) Receive() error {
+	errRead := readAndWrite(t.conn, t.out)
+	if errors.Is(errRead, io.EOF) {
+		errWrite := sendExitMessage(t.out, messageByClient)
+		if errWrite != nil {
+			return errWrite
+		}
+	}
+
+	return errRead
+}
+
+// Прочитать сообщение из входного источника, и записать его в выходной.
+func readAndWrite(in io.ReadCloser, out io.Writer) error {
+	buffer := make([]byte, 1024)
+	n, errRead := in.Read(buffer)
+	if hasBeenError(errRead) {
+		return fmt.Errorf("cannot read from input: %w", errRead)
+	}
+
+	_, errWrite := out.Write(buffer[:n])
+	if errWrite != nil {
+		return fmt.Errorf("cannot write to output: %w", errWrite)
+	}
+
+	return errRead
+}
+
+// Отправить сообщение об завершении сессии.
+func sendExitMessage(out io.Writer, message string) error {
+	_, errWrite := out.Write([]byte(message))
+	if errWrite != nil {
+		return errWrite
+	}
+
+	return nil
+}
