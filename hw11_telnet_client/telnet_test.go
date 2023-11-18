c057de559a159819b91ce31bcc3fc9063491575d
Ruslan-Androsenko <ruslan-developer@developerlab.pp.ru>
2023-11-18 03:25:15 +0300

    HW11 Added a test to check the mail server

--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io"
 	"net"
+	"regexp"
 	"sync"
 	"testing"
 	"time"
@@ -62,4 +63,79 @@ func TestTelnetClient(t *testing.T) {
 
 		wg.Wait()
 	})
+}
+
+func TestGoogleMailServer(t *testing.T) {
+	in := &bytes.Buffer{}
+	out := &bytes.Buffer{}
+
+	timeout, err := time.ParseDuration("10s")
+	require.NoError(t, err)
+
+	// Подключаемся к почтовому серверу
+	address := net.JoinHostPort("smtp.gmail.com", "587")
+	client := NewTelnetClient(address, timeout, io.NopCloser(in), out)
+	require.NoError(t, client.Connect())
+	defer func() { require.NoError(t, client.Close()) }()
+
+	err = client.Receive()
+	require.NoError(t, err)
+
+	pattern := "^220 smtp.gmail.com ESMTP (.*) - gsmtp\r\n$"
+	regExp, errRegExp := regexp.Compile(pattern)
+	require.NoError(t, errRegExp)
+
+	responseConnect := out.String()
+	require.Regexp(t, regExp, responseConnect)
+
+	// Извлекаем идентификатор сессии
+	regExp = regexp.MustCompile("220 smtp.gmail.com ESMTP ")
+	parts := regExp.Split(responseConnect, -1)
+	require.Len(t, parts, 2)
+
+	sessionID := parts[1]
+	require.NotEmpty(t, sessionID)
+
+	tests := []struct {
+		name     string
+		message  string
+		expected string
+	}{
+		{
+			name:     "Helo server",
+			message:  "HELO hellogoogle\r\n",
+			expected: "250 smtp.gmail.com at your service\r\n",
+		},
+		{
+			name:    "Ehlo server",
+			message: "EHLO smtp.gmail.com\r\n",
+			//nolint:lll
+			expected: "^250-smtp.gmail.com at your service, \\[.*\\]\r\n250-SIZE 35882577\r\n250-8BITMIME\r\n250-STARTTLS\r\n250-ENHANCEDSTATUSCODES\r\n250-PIPELINING\r\n250-CHUNKING\r\n250 SMTPUTF8\r\n$",
+		},
+		{
+			name:     "Quit connection server",
+			message:  "QUIT\r\n",
+			expected: "221 2.0.0 closing connection " + sessionID,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			out.Truncate(0)
+
+			// Записываем сообщение в сокет
+			in.WriteString(tc.message)
+			err = client.Send()
+			require.NoError(t, err)
+
+			// Получаем данные из сокета
+			err = client.Receive()
+			require.NoError(t, err)
+
+			regExp, errRegExp := regexp.Compile(tc.expected)
+			require.NoError(t, errRegExp)
+			require.Regexp(t, regExp, out.String())
+		})
+	}
 }
