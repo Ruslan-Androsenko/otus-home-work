9421aaf8acd2f6ea7ad2cfabd8829f8ee94cf37f
Ruslan-Androsenko <ruslan-developer@developerlab.pp.ru>
2024-05-14 08:29:50 +0300

    HW13 Corrected the typing of data that was passed through request parameters

--- hw12_13_14_15_calendar/internal/server/http/handlers.go
+++ hw12_13_14_15_calendar/internal/server/http/handlers.go
@@ -13,22 +13,22 @@ import (
 var ctx = context.Background()
 
 func (s *Server) createEventHandler(w http.ResponseWriter, r *http.Request) {
-	params, err := loadParams(r)
+	params, err := loadParams[EventParams](r)
 	if !errors.Is(err, io.EOF) {
 		logg.Errorf("%v", err)
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
 
-	event, errParse := makeCreateParams(params)
-	if errParse != nil {
+	event, errParse := params.getEvent()
+	if event == nil || errParse != nil {
 		logg.Errorf("Failed to parse create params: %v, Error: %v", params, errParse)
 		w.WriteHeader(http.StatusNotAcceptable)
 		return
 	}
 
-	if err = s.app.CreateEvent(ctx, event); err != nil {
-		logg.Errorf("Failed to create new event: %v, Error: %v", event, err)
+	if err = s.app.CreateEvent(ctx, *event); err != nil {
+		logg.Errorf("Failed to create new event: %v, Error: %v", *event, err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -39,22 +39,22 @@ func (s *Server) createEventHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) updateEventHandler(w http.ResponseWriter, r *http.Request) {
-	params, err := loadParams(r)
+	params, err := loadParams[EventParams](r)
 	if !errors.Is(err, io.EOF) {
 		logg.Errorf("%v", err)
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
 
-	eventID, event, errParse := makeUpdateParams(params)
-	if errParse != nil {
+	event, errParse := params.getEvent()
+	if event == nil || errParse != nil {
 		logg.Errorf("Failed to parse update params: %v, Error: %v", params, errParse)
 		w.WriteHeader(http.StatusNotAcceptable)
 		return
 	}
 
-	if err = s.app.UpdateEvent(ctx, eventID, event); err != nil {
-		logg.Errorf("Failed to update eventID: %s, event: %v, Error: %v", eventID, event, err)
+	if err = s.app.UpdateEvent(ctx, params.ID, *event); err != nil {
+		logg.Errorf("Failed to update eventID: %s, event: %v, Error: %v", params.ID, *event, err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -64,16 +64,15 @@ func (s *Server) updateEventHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) deleteEventHandler(w http.ResponseWriter, r *http.Request) {
-	params, err := loadParams(r)
+	params, err := loadParams[EventParams](r)
 	if !errors.Is(err, io.EOF) {
 		logg.Errorf("%v", err)
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
 
-	eventID := makeGetEventByIDParams(params)
-	if err = s.app.DeleteEvent(ctx, eventID); err != nil {
-		logg.Errorf("Failed to delete eventID: %v, Error: %v", eventID, err)
+	if err = s.app.DeleteEvent(ctx, params.ID); err != nil {
+		logg.Errorf("Failed to delete eventID: %v, Error: %v", params.ID, err)
 		w.WriteHeader(http.StatusNotAcceptable)
 		return
 	}
@@ -83,25 +82,26 @@ func (s *Server) deleteEventHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) getEventHandler(w http.ResponseWriter, r *http.Request) {
-	params, err := loadParams(r)
+	params, err := loadParams[EventParams](r)
 	if !errors.Is(err, io.EOF) {
 		logg.Errorf("%v", err)
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
 
-	eventID := makeGetEventByIDParams(params)
-	event, err := s.app.GetEventByID(eventID)
-	errIs := err.Error()
-	if errIs == storage.ErrSQLNoRows {
-		response := DataResponse{"state": "not found", "data": nil}
-		w.WriteHeader(http.StatusNotFound)
-		SendResponse(w, response, getEventMethod)
-		return
-	} else if err != nil {
-		logg.Errorf("Failed to get event by id: %v, Error: %v", eventID, err)
-		w.WriteHeader(http.StatusNotFound)
-		return
+	event, err := s.app.GetEventByID(params.ID)
+	if err != nil {
+		errIs := err.Error()
+		if errIs == storage.ErrSQLNoRows {
+			response := DataResponse{"state": "not found", "data": nil}
+			w.WriteHeader(http.StatusNotFound)
+			SendResponse(w, response, getEventMethod)
+			return
+		} else {
+			logg.Errorf("Failed to get event by id: %v, Error: %v", params.ID, err)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
 	}
 
 	response := DataResponse{"state": "success", "data": event}
@@ -126,14 +126,14 @@ func getEventsListByDate(
 	getEventsFn func(time time.Time) ([]storage.Event, error),
 	caption string,
 ) {
-	params, err := loadParams(r)
+	params, err := loadParams[EventParams](r)
 	if !errors.Is(err, io.EOF) {
 		logg.Errorf("%v", err)
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
 
-	eventDate, errParse := makeGetEventsByDateParams(params)
+	eventDate, errParse := params.getDate()
 	if errParse != nil {
 		logg.Errorf("Failed to parse get events params: %v, Error: %v", params, errParse)
 		w.WriteHeader(http.StatusNotAcceptable)
--- hw12_13_14_15_calendar/internal/server/http/request.go
+++ hw12_13_14_15_calendar/internal/server/http/request.go
@@ -6,14 +6,11 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
-
-	"github.com/Ruslan-Androsenko/otus-home-work/hw12_13_14_15_calendar/internal/storage"
 )
 
 // Получение параметров запроса.
-func loadParams(r *http.Request) (map[string]interface{}, error) {
-	var params map[string]interface{}
+func loadParams[T any](r *http.Request) (*T, error) {
+	var params T
 
 	defer func() {
 		if err := r.Body.Close(); err != nil {
@@ -33,78 +30,5 @@ func loadParams(r *http.Request) (map[string]interface{}, error) {
 		return nil, fmt.Errorf("failed to deserialize data: %s. Error: %w", string(data), errDecode)
 	}
 
-	return params, err
-}
-
-// Сформировать параметры для создания события.
-func makeCreateParams(params map[string]interface{}) (storage.Event, error) {
-	var (
-		event    storage.Event
-		errParse error
-	)
-
-	if val, ok := params["event"]; ok {
-		if eventParams, okCast := val.(map[string]interface{}); okCast {
-			event, errParse = storage.MakeEventFromParams(eventParams)
-		}
-	} else {
-		event, errParse = storage.MakeEventFromParams(params)
-	}
-
-	return event, errParse
-}
-
-// Сформировать параметры для обновления события.
-func makeUpdateParams(params map[string]interface{}) (string, storage.Event, error) {
-	var (
-		eventID  string
-		event    storage.Event
-		errParse error
-	)
-
-	if val, ok := params["id"]; ok {
-		if id, okCast := val.(string); okCast {
-			eventID = id
-		}
-	}
-
-	if val, ok := params["event"]; ok {
-		if eventParams, okCast := val.(map[string]interface{}); okCast {
-			event, errParse = storage.MakeEventFromParams(eventParams)
-		}
-	}
-
-	return eventID, event, errParse
-}
-
-// Сформировать параметры для получения события по его ID.
-func makeGetEventByIDParams(params map[string]interface{}) string {
-	var eventID string
-
-	if val, ok := params["id"]; ok {
-		if id, okCast := val.(string); okCast {
-			eventID = id
-		}
-	}
-
-	return eventID
-}
-
-// Сформировать параметры для получения списка событий по дате.
-func makeGetEventsByDateParams(params map[string]interface{}) (time.Time, error) {
-	var (
-		date     time.Time
-		errParse error
-	)
-
-	if val, ok := params["date"]; ok {
-		if eventDate, okCast := val.(string); okCast {
-			date, errParse = time.ParseInLocation(storage.DateFormat, eventDate, time.Local)
-			if errParse != nil {
-				return time.Time{}, errParse
-			}
-		}
-	}
-
-	return date, errParse
+	return &params, err
 }
